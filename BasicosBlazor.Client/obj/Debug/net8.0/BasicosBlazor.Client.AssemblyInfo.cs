364804ea503feb45521bf67a70e90524cf324f31
José Quiñónez <joserobertoq@outlook.com>
2024-07-05 05:33:37 -0600

    Add Persona Page

--- BasicosBlazor.Client/obj/Debug/net8.0/BasicosBlazor.Client.AssemblyInfo.cs
+++ BasicosBlazor.Client/obj/Debug/net8.0/BasicosBlazor.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BasicosBlazor.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e84d063c43c99323a384f7c6d73e2960fc124236")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2b10e55255b19fd0e9c3c0928f1b488b1cf9295")]
 [assembly: System.Reflection.AssemblyProductAttribute("BasicosBlazor.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BasicosBlazor.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
