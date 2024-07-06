1243d97e587fac54ca8d99e104d357dd31796553
José Quiñónez <joserobertoq@outlook.com>
2024-07-06 06:00:40 -0600

    Add Validations

--- BasicosBlazor.Library/CarreraCLS.cs
+++ BasicosBlazor.Library/CarreraCLS.cs
@@ -1,10 +1,14 @@
-﻿namespace BasicosBlazor.Library;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BasicosBlazor.Library;
 
 public class CarreraCLS
 {
     public int idcarrera { get; set; }
 
+    [Required(ErrorMessage = "El nombre de la carrera es obligatorio")]
     public string nombrecarrera { get; set; }
 
+    [Required(ErrorMessage = "La descripción de la carrera es obligatoria")]
     public string descripcioncarrera { get; set; }
 }
--- BasicosBlazor.Library/obj/Debug/net8.0/BasicosBlazor.Library.AssemblyInfo.cs
+++ BasicosBlazor.Library/obj/Debug/net8.0/BasicosBlazor.Library.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BasicosBlazor.Library")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85b5149c2b495128e4339253d8cb13eb60bddc69")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c8cdf5057c441b87922b326c9f47be9620840b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("BasicosBlazor.Library")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BasicosBlazor.Library")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
